9be041de9400bfc76b542269439aa85fd1dcb127
Jacky Jin <butfly824@gmail.com>
2020-07-06 20:00:41 +0800

    Update get a quote

--- src/app/get-quote/get-quote.component.ts
+++ src/app/get-quote/get-quote.component.ts
@@ -1,15 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import $ from 'jquery';
 import {NgxSpinnerService} from 'ngx-spinner';
-
-
-import { ProfileService } from '../profile.service';
-import { SnotifyService } from 'ng-snotify';
-import { Model } from './getaquote_model';
-import { NgForm } from '@angular/forms';
-
 import * as Email from '../../assets/js/smtp';
-declare let Email: any;
 
 
 @Component({
@@ -18,8 +10,8 @@ declare let Email: any;
   styleUrls: ['./get-quote.component.less']
 })
 export class GetQuoteComponent implements OnInit {
-  model: Model = new Model();
-  constructor( private profile: ProfileService, private snotify: SnotifyService, private spinner: NgxSpinnerService) { }
+
+  constructor(private spinner: NgxSpinnerService) { }
 
   handleMenuOpenClick() {
     $('.menu').stop(0).fadeIn('fast');
@@ -30,20 +22,17 @@ export class GetQuoteComponent implements OnInit {
   handleSelectAttachmentClick() {
     $('.select-attachment').trigger('click');
   }
-
-  onSubmit(f: NgForm) {
-
+  sendEmail() {
+    console.log(Email);
     Email.send({
-    Host : 'smtp.elasticemail.com',
-    Username : 'marianbx824@gmail.com',
-    Password : '4C477F300CFD840C12C4B5AB03CB5ACCDD2E',
-    To : 'marianbx824@gmail.com',
-    From : 'marianbx824@gmail.com',
-    Subject : this.model.subject,
-    Body : `
-    <i>This is sent as a feedback from my resume page.</i> <br/> <b>Name: </b>${this.model.name} <br /> <b>Email: </b>${this.model.email}<br /> <b>Subject: </b>${this.model.subject}<br /> <b>Message:</b> <br /> ${this.model.message} <br><br> <b>~End of Message.~</b> `
-    }).then( message => {alert(message); f.resetForm(); } );
-      
+      Host : 'smtp.mailtrap.io',
+      Username : 'faf0bceaef7351',
+      Password : '609a1da089e057',
+      To : 'maikap.samir14@gmail.com',
+      From : 'maikap.samir@gmail.com',
+      Subject : 'this.model.subject',
+      Body : 'wfewfwefwewef',
+    }).then( message => { alert(message); });
   }
 
   ngOnInit(): void {
