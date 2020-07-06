7ed1662a145b70c2d4cc1357f1d8a917fc651737
Jacky Jin <butfly824@gmail.com>
2020-07-06 19:56:57 +0800

    Update get a quote

---
+++ src/app/get-quote/getaquote_model.ts
@@ -0,0 +1,11 @@
+export class Model {
+    name:string;
+    email:string;
+    phone: string;
+    message: string;
+    whatdoyouneed:string;
+    brandIdentity:string;
+    budget:string;
+    checkterm:string;
+    subject:string;
+}
