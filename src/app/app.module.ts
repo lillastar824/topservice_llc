fe3e1a80f30d121be6a5de98833c9f8f46dd07dd
JackOfAll <butfly824@gmail.com>
2021-12-06 05:27:11 -0500

    Update header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,11 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent,
   ],
   imports: [
     BrowserModule,
