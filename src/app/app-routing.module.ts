bdcacacdbe653d7b8aba8cfc8b489db46d109418
JackOfAll <butfly824@gmail.com>
2021-12-06 05:22:45 -0500

    removed the unnecessary files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,75 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { GetQuoteComponent } from "./get-quote/get-quote.component";
 import { HomeComponent } from "./home/home.component";
-import { PortfolioComponent } from "./portfolio/portfolio.component";
-import { CupidComponent } from "./cupid/cupid.component";
-import { SufferComponent } from "./suffer/suffer.component";
-import { DietComponent } from "./diet/diet.component";
-import { BoltComponent } from "./bolt/bolt.component";
-import { LyricalComponent } from "./lyrical/lyrical.component";
-import { ContactComponent } from "./contact/contact.component";
-import { AboutComponent } from "./about/about.component";
 
 const routes: Routes = [
   {
     path: "",
     component: HomeComponent,
     data: { title: "Top Service" },
-    pathMatch: "full"
-  },
-  {
-    path: "get-quote",
-    component: GetQuoteComponent,
-    data: { title: "Top Service - Get Quote" },
-    pathMatch: "full"
-  },
-  {
-    path: "portfolio",
-    component: PortfolioComponent,
-    data: { title: "Top Service - Portfolios" },
-    pathMatch: "full"
-  },
-  {
-    path: "bolt",
-    component: BoltComponent,
-    data: { title: "Top Service - Portfolios" },
-    pathMatch: "full"
-  },
-  {
-    path: "lyrical",
-    component: LyricalComponent,
-    data: { title: "Top Service - Portfolios" },
-    pathMatch: "full"
-  },
-  {
-    path: "cupid",
-    component: CupidComponent,
-    data: { title: "Top Service - Portfolios" },
-    pathMatch: "full"
-  },
-  {
-    path: "suffer",
-    component: SufferComponent,
-    data: { title: "Top Service - Portfolios" },
-    pathMatch: "full"
-  },
-  {
-    path: "diet",
-    component: DietComponent,
-    data: { title: "Top Service - Portfolios" },
-    pathMatch: "full"
-  },
-  {
-    path: "about",
-    component: AboutComponent,
-    data: { title: "Top Service - About" },
-    pathMatch: "full"
-  },
-  {
-    path: "contact",
-    component: ContactComponent,
-    data: { title: "Top Service - Contact" },
     pathMatch: "full"
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,33 +6,14 @@ import { NgxSpinnerModule } from "ngx-spinner";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
-import { GetQuoteComponent } from "./get-quote/get-quote.component";
-import { PortfolioComponent } from "./portfolio/portfolio.component";
-
-import { ContactComponent } from "./contact/contact.component";
-import { AboutComponent } from "./about/about.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HeaderComponent } from "./header/header.component";
-import { CupidComponent } from './cupid/cupid.component';
-import { DietComponent } from './diet/diet.component';
-import { SufferComponent } from './suffer/suffer.component';
-import { BoltComponent } from './bolt/bolt.component';
-import { LyricalComponent } from './lyrical/lyrical.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    GetQuoteComponent,
-    PortfolioComponent,
-    ContactComponent,
-    AboutComponent,
     HeaderComponent,
-    CupidComponent,
-    DietComponent,
-    SufferComponent,
-    BoltComponent,
-    LyricalComponent
   ],
   imports: [
     BrowserModule,
